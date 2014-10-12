2d772f9bf1f224d3bc16d85769e15df0af2fb93d
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-10-11 22:10:07 -0700

    first draft of list test

--- test.js
+++ test.js
@@ -1,10 +1,37 @@
-var http = require('http');
-http.get(process.argv[2], function(res) {
-  res.setEncoding('utf8');
-  res.on("data", function (data) {
-    console.log(data);
+//* Test async i/o
+//  var fs = require('fs');
+// fs.readFile(process.argv[2], { encoding: 'utf8' }, function(err, contents) {
+//       if (err) { lcb(err); return; }
+//       // return contents.toString().split('\n');
+//       console.log(contents.toString().split('\n').length-1);
+//     });
+
+//* Test filter ls
+var fs = require('fs');
+fs.readdir(process.argv[2], function(err, files) {
+  if (err) { return finish(err); }
+  if (files.length === 0) { finish(); }
+  files.forEach(function(file) {
+    fs.readFile(path.join(dir, file), { encoding: 'utf8' }, function(err, fileContents) {
+      if (err) { return finish(err); }
+      contents[file] = fileContents;
+      console.log(fileContents);
+      if (Object.keys(contents).length === files.length) {
+        finish();
+      }
+    });
   });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
 });
+
+
+//* Test http client
+// var http = require('http');
+// http.get(process.argv[2], function(res) {
+//   res.setEncoding('utf8');
+//   res.on("data", function (data) {
+//     console.log(data);
+//   });
+// }).on('error', function(e) {
+//   console.log("Got error: " + e.message);
+// });
 
